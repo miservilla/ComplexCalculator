4533998914d8d4bdc7783d0a10807cb6a4f08e55
Michael John Servilla <chico@unm.edu>
2016-05-16 21:24:39 -0600

    Initial commit

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,57 @@
+package com.example.java;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String operation;
+        Double result;
+        System.out.print("This is a complex calculator, please enter the first number: ");
+        double firstNumber = TryParseDouble();
+        System.out.print("This is a complex calculator, please enter the second number: ");
+        double secondNumber = TryParseDouble();
+        System.out.print("Please enter one operation (-, +, /, *): ");
+        Scanner op = new Scanner(System.in);
+        operation = op.next();
+        while (!operation.equals("-") && !operation.equals("+") && !operation.equals("/") && !operation.equals("*")){
+            System.out.println("Can not parse the entered operation, please try again!");
+            operation = op.next();
+        }
+        switch (operation){
+            case "-":
+                result = firstNumber - secondNumber;
+                System.out.println("Your answer is: " + result);
+                break;
+            case "+":
+                result = firstNumber + secondNumber;
+                System.out.println("Your answer is: " + result);
+                break;
+
+            case "/":
+                result = firstNumber / secondNumber;
+                System.out.println("Your answer is: " + result);
+                break;
+
+            default:
+                result = firstNumber * secondNumber;
+                System.out.println("Your answer is: " + result);
+
+        }
+
+
+    }
+
+    public static Double TryParseDouble(){
+        Scanner sc = new Scanner(System.in);
+        Double number;
+//        sc.next();
+        while (!sc.hasNextDouble()) {
+            System.out.println("Can not parse, please enter real number!");
+            sc.next();
+        }
+        number = sc.nextDouble();
+        return number;
+
+    }
+}
